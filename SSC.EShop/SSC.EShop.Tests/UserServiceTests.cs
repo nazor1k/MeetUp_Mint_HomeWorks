1b5098ef3643a35910122be3d3fe91a1b7d44bcc
Назар Коробчук <nazarkorobchuk08@gmail.com>
2025-11-25 01:02:21 +0200

    Add SSC.EShop.Tests project with initial UserService tests
    Added a `Tests` solution folder and a new `SSC.EShop.Tests` project
    targeting `net9.0` with references to `NUnit`, `Moq`, and
    `Microsoft.NET.Test.Sdk`. Updated `SSC.EShop.sln` to include build
    configurations and nested the test project under the `Tests` folder.
    
    Implemented `UserServiceTests` with a `Setup` method for mocking
    dependencies (`IUserRepository`, `IUnitOfWork`, `IAppLogger`) and
    added a test method `RegisterAsync_ShouldReturnTrue_WhenUserIsNew`
    to validate `UserService.RegisterAsync` behavior.

---
+++ SSC.EShop/SSC.EShop.Tests/UserServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using Moq;
+using SSC.Eshop.Business.DTOs;
+using SSC.Eshop.Business.Services.UserService;
+using SSC.EShop.Core.Entities;
+using SSC.EShop.Core.Interfaces;
+using System.Linq.Expressions;
+using NUnit.Framework.Legacy;
+
+namespace SSC.EShop.Tests
+{
+    public class UserServiceTests
+    {
+        private Mock<IUserRepository> _userRepositoryMock;
+        private Mock<IUnitOfWork> _unitOfWorkMock;
+        private Mock<IAppLogger<UserService>> _loggerMock;
+
+        private UserService _userService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _loggerMock = new Mock<IAppLogger<UserService>>();
+
+            _userService = new UserService(
+                _userRepositoryMock.Object,
+                _loggerMock.Object,
+                _unitOfWorkMock.Object
+            );
+        }
+
+        [Test]
+        public async Task RegisterAsync_ShouldReturnTrue_WhenUserIsNew()
+        {
+            // Arrange (Підготовка)
+            var dto = new CreateUserDto { Login = "new_user", FirstName = "Test", Password = "123" };
+
+            _userRepositoryMock
+                .Setup(repo => repo.GetByConditionAsync(It.IsAny<Expression<Func<User, bool>>>()))
+                .ReturnsAsync(new List<User>());
+
+            // Act (Дія)
+            var result = await _userService.RegisterAsync(dto);
+
+            // Assert (Перевірка)
+            Assert.That(result, Is.True);
+
+            
+            _userRepositoryMock.Verify(repo => repo.InsertAsync(It.IsAny<User>()), Times.Once);
+            _unitOfWorkMock.Verify(uow => uow.SaveChangesAsync(), Times.Once);
+        }
+    }
+}
